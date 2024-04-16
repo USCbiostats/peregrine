0ea9e01d3293bfb59e3854a5ba72876c420c5664
akshala <akshala.bhatnagar@gmail.com>
2024-04-16 14:36:27 -0700

    eQTL folder refactor changes

--- steps.sh
+++ steps.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+ echo "ERROR: Please specify target folder path"
+ exit 1
+fi
+TARGET=$1
+
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 echo 'venv done'
 
-cd eQTL
-python eqtl.py
+python eQTL/eqtl.py $TARGET/eQTL
 echo 'eQTL done'
 
-cd ../heirarchicalTAD
-python heirarchicalTAD.py
-echo 'heirarchical TAD done'
+# python heirarchicalTAD/heirarchicalTAD.py $TARGET/heirarchicalTAD
+# echo 'heirarchical TAD done'
 
-cd ../tad
-./tad.sh
-echo 'TAD done'
+# ./tad/tad.sh $TARGET/tad
+# echo 'TAD done'
 
 # cd ../chia_pet
 # python ChIA_PET.py
